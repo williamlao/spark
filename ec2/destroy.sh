74a36f498f888ac384d340ee87a569720faf9c2e
patrick-blockseer <patrick@blockseer.com>
2017-12-08 11:42:43 -0800

    uncomment scripts and change repos to patrick-blockseer owned repos

--- ec2/destroy.sh
+++ ec2/destroy.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 
 #destroy
-#./spark-ec2 -k spark -i ~/.ssh/spark.pem --region=us-west-2 --zone=us-west-2b destroy sparkcluster
-
+./spark-ec2 -k patrick-spark -i ~/.ssh/patrick-spark.pem --region=us-west-2 --zone=us-west-2b destroy sparkcluster
