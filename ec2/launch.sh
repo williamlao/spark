9a3dd78289bc709237b59ec4a74fb30d508380aa
patrick-blockseer <patrick@blockseer.com>
2017-12-11 16:46:17 -0800

    more volume

--- ec2/launch.sh
+++ ec2/launch.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 # launch
-./spark-ec2 -k patrick-spark -i ~/.ssh/patrick-spark.pem --slaves 4 --instance-type r3.large --ebs-master-vol-size=150 --ebs-vol-size=50 --ebs-vol-type=gp2 --region=us-west-2 --zone=us-west-2b --spark-ec2-git-repo=https://github.com/patrick-blockseer/spark-ec2 --spark-ec2-git-branch=branch-1.4-persistent launch sparkcluster
+./spark-ec2 -k patrick-spark -i ~/.ssh/patrick-spark.pem --slaves 4 --instance-type r3.large --ebs-master-vol-size=6000 --ebs-vol-size=200 --ebs-vol-type=gp2 --region=us-west-2 --zone=us-west-2b --spark-ec2-git-repo=https://github.com/patrick-blockseer/spark-ec2 --spark-ec2-git-branch=branch-1.4-persistent launch sparkcluster
